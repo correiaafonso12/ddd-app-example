ea87fed6e53c71dbdfeae834a7ebf2480722f10e
Afonso Correia <1190329@isep.ipp.pt>
2025-03-15 16:48:48 +0000

    feat: add standalone Mapper lib

---
+++ gorgeous-sandwiches/common/mapper/src/main/java/app/ddd/gsandwiches/mapper/Mapper.java
@@ -0,0 +1,29 @@
+package app.ddd.gsandwiches.mapper;
+
+/**
+ * Mapper to translate from Source class to Target class
+ */
+public interface Mapper<S, T> {
+
+    /**
+     * Get source type
+     * 
+     * @return Source type class
+     */
+    Class<S> sourceType();
+
+    /**
+     * Get target type
+     * 
+     * @return Target type class
+     */
+    Class<T> targetType();
+
+    /**
+     * Maps source object to target object
+     * 
+     * @param source Source object to be mapped
+     * @return Transformed target object
+     */
+    T map(S source);
+}
